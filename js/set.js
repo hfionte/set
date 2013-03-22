beef6a39402be94d3ca14c4ceeb2b451f555af12
Holly Fionte <hfionte@Hollys-MacBook-Pro.local>
2013-03-22 14:09:59 -0400

    initial commit

---
+++ js/set.js
@@ -0,0 +1,101 @@
+/* SET */
+
+$('document').ready(function () {
+
+	// Build the deck... total of 81 cards.
+	// Each card has 3 options for number, color, shape, and texture
+	var numberOptions = ["one", "two", "three"];
+	var colorOptions = ["red", "green", "purple"];
+	var shapeOptions = ["diamond", "oval", "squiggle"];
+	var textureOptions = ["solid", "transparent", "striped"];
+	var cardFeatures = [numberOptions, colorOptions, shapeOptions, textureOptions];
+
+	// Build combinations
+	var BASE = 3, LEN = 4, LIMIT = Math.round(Math.pow(BASE, LEN));
+	var featureSets = [];
+	for (var i = 0; i < LIMIT; ++i) {
+	  var item = [];
+	  for (var j = 0, k = i; j < LEN; ++j, k = Math.floor(k/BASE)) {
+	    item.push(k % BASE);
+	  }
+	  featureSets.push(item);
+	}
+
+	// Build cards
+	for ( var i=0; i<featureSets.length; i++ ) {
+		var newCard = document.createElement('div');
+		var featureSet = featureSets[i];
+		$(newCard).addClass('card')
+			.attr('data-number', numberOptions[featureSet[0]]).append(numberOptions[featureSet[0]] + "<br>")
+			.attr('data-color', colorOptions[featureSet[1]]).append(colorOptions[featureSet[1]] + "<br>")
+			.attr('data-shape', shapeOptions[featureSet[2]]).append(shapeOptions[featureSet[2]] + "<br>")
+			.attr('data-texture', textureOptions[featureSet[3]]).append(textureOptions[featureSet[3]] + "<br>");
+		$('#deck').append(newCard);
+	}
+
+	// Draw a card
+	function drawCard() {
+		var deckSize = $('#deck .card').length;
+		var drawNumber = Math.ceil((Math.random() * deckSize) - 1);
+		var card = $('#deck .card').get(drawNumber);
+		$(card).appendTo('#card-table');
+	}
+
+	// Set up table
+	for ( var i=0; i<9; i++ ) {
+		drawCard();
+	}
+
+	// Select cards to check
+	$(document).on( 'click', '#card-table .card', function () {
+		if ($(this).hasClass('selected')) {
+			$(this).removeClass('selected');
+		} else if ($('.selected').length < 3) {
+			$(this).toggleClass('selected');
+		}
+	});
+
+	// Check selected set
+	$(document).on( 'click', '#check-set', function (){
+		if ($('.selected').length == 3) {
+			var $set = $('.selected');
+			
+			$set.each(function () {
+				var myNumber = $(this).attr('data-number');
+				numberSet = $(this).siblings('.selected[data-number="' + myNumber + '"]').length == 0 || $(this).siblings('.selected[data-number="' + myNumber + '"]').length == 2 ;
+
+				var myColor = $(this).attr('data-color');
+				colorSet = $(this).siblings('.selected[data-color="' + myColor + '"]').length == 0 || $(this).siblings('.selected[data-color="' + myColor + '"]').length == 2 ;
+
+				var myShape = $(this).attr('data-shape');
+				shapeSet = $(this).siblings('.selected[data-shape="' + myShape + '"]').length == 0 || $(this).siblings('.selected[data-shape="' + myShape + '"]').length == 2 ;
+
+				var myTexture = $(this).attr('data-texture');
+				textureSet = $(this).siblings('.selected[data-texture="' + myTexture + '"]').length == 0 || $(this).siblings('.selected[data-texture="' + myTexture + '"]').length == 2 ;
+			});
+
+			if ( numberSet && colorSet && shapeSet && textureSet ) {
+				alert('Set!');
+				$set.appendTo('#discard-pile');
+				if ( $('#card-table .card').length < 9 ) {
+					for ( var i=0; i<3; i++ ) {
+						drawCard();
+					}
+				}
+			} else {
+				alert('Nope!');
+			}
+
+		} else {
+			alert("Please select 3 cards.");
+		}
+	});
+
+	// Draw more 
+	$(document).on( 'click', '#no-sets', function (){
+		for ( var i=0; i<3; i++ ) {
+			drawCard();
+		}
+	});
+
+});
